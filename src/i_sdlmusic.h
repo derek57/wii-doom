bc01a02765ef38d49925598b789f3fbf66586de5
Ronald Lasmanowicz <derek57@users.noreply.github.com>
2015-05-19 02:42:09 +0200

    - add missing headers

---
+++ src/i_sdlmusic.h
@@ -0,0 +1,26 @@
+//
+// Copyright(C) 2005-2014 Simon Howard
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//
+//
+
+#ifndef __I_SDLMUSIC_H__
+#define __I_SDLMUSIC_H__
+
+#include "doomtype.h"
+
+void I_SDL_PollMusic(void);
+
+#endif /* #ifndef __I_SDLMUSIC_H__ */
+
---
+++ src/i_wiimain.h
@@ -0,0 +1,6 @@
+#ifndef __I_WIIMAIN_H__
+#define __I_WIIMAIN_H__
+
+int wii_main();
+
+#endif
