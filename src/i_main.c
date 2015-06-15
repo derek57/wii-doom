be7c63361c920150e494b74c8ea8e00b0e3c2dcf
Ronald Lasmanowicz <derek57@users.noreply.github.com>
2015-06-15 20:24:57 +0200

    - reset debug settings

--- src/i_main.c
+++ src/i_main.c
@@ -37,13 +37,13 @@
 
 
 // MAIN DEVPARM
-boolean        devparm = true;
+boolean        devparm = false;
 boolean        devparm_net = false;
 
 // SOLO DEVPARM
 boolean        devparm_nerve = false;
 boolean        devparm_doom = false;
-boolean        devparm_doom2 = true;
+boolean        devparm_doom2 = false;
 boolean        devparm_freedoom2 = false;
 boolean        devparm_tnt = false;
 boolean        devparm_plutonia = false;
