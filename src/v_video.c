d4ca57b281d65155479358a3160ea39ee740a33c
Ronald Lasmanowicz <derek57@users.noreply.github.com>
2015-06-30 10:55:41 +0200

    - forgot one

--- src/v_video.c
+++ src/v_video.c
@@ -5,6 +5,8 @@
 // Copyright(C) 1993-2008 Raven Software
 // Copyright(C) 2005 Simon Howard
 //
+// Copyright(C) 2015 by Brad Harding: - (Video Drawing Functions for Console & Status Bar)
+//
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
