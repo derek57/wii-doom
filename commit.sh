05243532cce9e69935cb7cd96246bc2bd8b0f464
Ronald Lasmanowicz <derek57@users.noreply.github.com>
2016-01-27 13:14:22 +0100

    - add missing file...

---
+++ commit.sh
@@ -0,0 +1,42 @@
+cd man
+cp Makefile.* /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/man
+cd ..
+cd opl
+cp Makefile.* /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/opl
+cp *.c /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/opl
+cp *.h /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/opl
+cd ..
+cd pcsound
+cp Makefile.* /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/pcsound
+cp *.c /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/pcsound
+cp *.h /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/pcsound
+cd ..
+cd src
+cp Makefile.* /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/src
+cp *.c /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/src
+cp *.h /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/src
+cd doom
+cp Makefile.* /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/src/doom
+cp *.c /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/src/doom
+cp *.h /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/src/doom
+cd ..
+cd ..
+cd textscreen
+cp Makefile.* /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/textscreen
+cp *.c /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/textscreen
+cp *.h /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/textscreen
+cd ..
+cd wii
+cp *.c /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/wii
+cp *.h /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/wii
+cp *.jpg /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/wii
+cp *.s /home/user/GitHub/WIIDOOM/wii-doom.git/trunk/wii
+cd ..
+cp Makefile.* /home/user/GitHub/WIIDOOM/wii-doom.git/trunk
+cp *.sh /home/user/GitHub/WIIDOOM/wii-doom.git/trunk
+cp config.h /home/user/GitHub/WIIDOOM/wii-doom.git/trunk
+cd ..
+cd GitHub
+cd WIIDOOM
+cd wii-doom.git
+svn ci *
